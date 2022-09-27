28c841ecda12e35e895404c7dd28ad6f47e8300b
Allan Grevas <stratosgrevas@gmail.com>
2022-09-27 01:05:16 -0300

    arquivo server js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+// definição da porta padrão da api
+const port = 3000;
+ 
+// configuração para aceitar requisições de qualquer origem
+app.use(cors());
+
+// configuração para retornar tudo em JSON
+app.use(express.json());
+
+//iniciando o servidor
+app.listen(port);
+console.log('API funcionando!');
