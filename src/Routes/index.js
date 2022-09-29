dfc8e2ca4e24a3de4c4c5d2b4023c52bcb2164ff
Allan Grevas <stratosgrevas@gmail.com>
2022-09-29 18:57:06 -0300

    exclusao de arquivos para refatoracao de codigo

--- src/Routes/index.js
+++
@@ -1,5 +0,0 @@
-// definição de todas as rotas da api
-const TodoRoute = require('./TodoRoute');
-module.exports = (app) => {
-   TodoRoute(app) // aqui vamos liberar o acesso às rotas do recurso TODO
-}
