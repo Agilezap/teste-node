39ad67070f8069614ed60c6eafbbca6292c93de3
Allan Grevas <stratosgrevas@gmail.com>
2022-09-29 19:01:57 -0300

    config conexao banco de dados

--- config.js
+++ config.js
@@ -1,12 +1,17 @@
-// configuração da conexão com o banco de dados
-const config = {
-    db: {
-      host: "localhost", // servidor do banco de dados
-      user: "root", // usuário do banco de dados
-      password: "", // senha do banco de dados
-      database: "todo", // banco de dados
-    },
-  };
+/**
+ * configuração da conexão com o banco de dados
+ * Autor: Allan Gleison Santos
+ */
 
-  // exportando esse módulo para acesso em outros arquivos
-  module.exports = config;
+const config = {
+  db: {
+    host: "localhost", // servidor do banco de dados
+    user: "root", // usuário do banco de dados
+    password: "", // senha do banco de dados
+    database: "todo", // banco de dados
+  },
+  listPerPage: 10,
+};
+
+// exportando esse módulo para acesso em outros arquivos
+module.exports = config;
